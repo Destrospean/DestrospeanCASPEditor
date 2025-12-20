083b6b848c754d2c3b478cc476b290cf0d48b23d
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2025-12-20 16:35:30 -0600

    Remove primitive logging

--- DestrospeanCASPEditor/Program.cs
+++ DestrospeanCASPEditor/Program.cs
@@ -4,18 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            try
-            {
-                Gtk.Application.Init();
-                new MainWindow();
-                Gtk.Application.Run();
-            }
-            catch (System.Exception ex)
-            {
-                var directoryPath = System.IO.Path.GetDirectoryName(ApplicationSpecificSettings.SettingsFilePath);
-                System.IO.Directory.CreateDirectory(directoryPath);
-                System.IO.File.WriteAllText(directoryPath + System.IO.Path.DirectorySeparatorChar + "crash-" + System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt", ex.ToString());
-            }
+            Gtk.Application.Init();
+            new MainWindow();
+            Gtk.Application.Run();
         }
     }
 }
