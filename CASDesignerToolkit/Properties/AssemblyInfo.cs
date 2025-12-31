628746b4f1713983ccf776faaba3dee9cf92e3ca
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2025-12-31 13:10:45 -0600

    Rename folder

--- CASDesignerToolkit/Properties/AssemblyInfo.cs
+++ CASDesignerToolkit/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("DestrospeanCASPEditor")]
+[assembly: AssemblyTitle("CASDesignerToolkit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DestrospeanCASPEditor")]
+[assembly: AssemblyProduct("CASDesignerToolkit")]
 [assembly: AssemblyCopyright("Copyright 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
