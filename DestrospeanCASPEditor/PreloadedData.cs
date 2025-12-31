f91653b1fa69836133f6df24de39af15db2f58c7
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2025-12-31 09:03:12 -0600

    Minor change

--- DestrospeanCASPEditor/PreloadedData.cs
+++ DestrospeanCASPEditor/PreloadedData.cs
@@ -8,13 +8,8 @@ namespace Destrospean.DestrospeanCASPEditor
 
         public readonly Dictionary<string, CmarNYCBorrowed.GEOM> GEOMs = new Dictionary<string, CmarNYCBorrowed.GEOM>(System.StringComparer.InvariantCultureIgnoreCase);
 
-        public static PreloadedData Singleton;
+        public static PreloadedData Singleton = new PreloadedData();
 
         public readonly Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource> VPXYs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase);
-
-        static PreloadedData()
-        {
-            Singleton = new PreloadedData();
-        }
     }
 }
