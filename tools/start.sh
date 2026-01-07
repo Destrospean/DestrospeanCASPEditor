0499d6ec6f2958aff408e43875814c854efc380f
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-01-07 15:13:54 -0600

    Fix start.sh

--- tools/start.sh
+++ tools/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "${0%/*}"
-export MONO_REGISTRY_PATH=registry
+export MONO_REGISTRY_PATH=registry LIBGL_ALWAYS_SOFTWARE=true GALLIUM_DRIVER=llvmpipe GTK_PATH=$HOME/.steam/steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/gtk-2.0 LD_PRELOAD=$HOME/.steam/steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/libgtk-x11-2.0.so.0:$HOME/.steam/steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/libgdk-x11-2.0.so.0 LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 if [ ! -d $MONO_REGISTRY_PATH ]; then mkdir registry; fi
-LIBGL_ALWAYS_SOFTWARE=true GALLIUM_DRIVER=llvmpipe GTK_PATH=$HOME/.steam/steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/gtk-2.0 LD_PRELOAD=$HOME/.steam/steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/libgtk-x11-2.0.so.0:$HOME/.steam/steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/libgdk-x11-2.0.so.0 LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH ./CASDesignerToolkit "$1"
+if [ -z "$1" ]; then ./CASDesignerToolkit; else ./CASDesignerToolkit "$1"; fi
